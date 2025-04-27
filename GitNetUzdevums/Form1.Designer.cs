8c17996df5686aee58e60d27823e9605c6a867bb
janisli <janis01li@inbox.lv>
2025-04-27 19:14:39 +0300

    Pievienota Jāņa Lietavieša hiperbolas funkcija

--- GitNetUzdevums/Form1.Designer.cs
+++ GitNetUzdevums/Form1.Designer.cs
@@ -32,8 +32,11 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.janisLietavietis = new System.Windows.Forms.GroupBox();
+            this.jlHiperbola = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.janisLietavietis.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -75,17 +78,39 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Andrejs Liepiņš";
             // 
+            // janisLietavietis
+            // 
+            this.janisLietavietis.Controls.Add(this.jlHiperbola);
+            this.janisLietavietis.Location = new System.Drawing.Point(358, 116);
+            this.janisLietavietis.Name = "janisLietavietis";
+            this.janisLietavietis.Size = new System.Drawing.Size(98, 56);
+            this.janisLietavietis.TabIndex = 4;
+            this.janisLietavietis.TabStop = false;
+            this.janisLietavietis.Text = "Jānis Lietavietis";
+            // 
+            // jlHiperbola
+            // 
+            this.jlHiperbola.Location = new System.Drawing.Point(6, 19);
+            this.jlHiperbola.Name = "jlHiperbola";
+            this.jlHiperbola.Size = new System.Drawing.Size(64, 29);
+            this.jlHiperbola.TabIndex = 0;
+            this.jlHiperbola.Text = "y = 1 / x";
+            this.jlHiperbola.UseVisualStyleBackColor = true;
+            this.jlHiperbola.Click += new System.EventHandler(this.jlHiperbola_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1016, 415);
+            this.Controls.Add(this.janisLietavietis);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Funkciju grafiki";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.groupBox1.ResumeLayout(false);
+            this.janisLietavietis.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -96,6 +121,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox janisLietavietis;
+        private System.Windows.Forms.Button jlHiperbola;
     }
 }
 
